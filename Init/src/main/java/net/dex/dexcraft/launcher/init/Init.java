af9a9990c52587bead9839a88c811fd16743b21d
daniel-almeida1989 <dex.houshi@hotmail.com>
2020-12-26 01:28:18 -0300

    v2.1.3-201225-601
    DexCraft Commons classes modifications and adjusts, see README for details.

--- Init/src/main/java/net/dex/dexcraft/launcher/init/Init.java
+++ Init/src/main/java/net/dex/dexcraft/launcher/init/Init.java
@@ -11,6 +11,7 @@ import javafx.application.Application;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
@@ -20,7 +21,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import net.dex.dexcraft.commons.check.AdminExecution;
+import net.dex.dexcraft.commons.Commons;
 import net.dex.dexcraft.commons.check.OfflineMode;
 import net.dex.dexcraft.commons.check.SystemRequirements;
 import net.dex.dexcraft.commons.tools.Close;
@@ -29,12 +30,13 @@ import net.dex.dexcraft.commons.tools.DexUI;
 import net.dex.dexcraft.commons.tools.Download;
 import net.dex.dexcraft.commons.tools.ErrorAlerts;
 import net.dex.dexcraft.commons.tools.Logger;
+import net.dex.dexcraft.launcher.init.services.Validate;
 
 
 /**
   * @author Dex
   * @since 30/04/2020
-  * @version v2.1.2-201213-595
+  * @version v2.1.3-201225-601
   *
   * Preloader Class with splash screen.
   */
@@ -44,19 +46,25 @@ public class Init extends Application
   static Stage mainWindowStage =  new Stage();
   static Label preloaderLabel;
   static ProgressBar pbar;
-  static ErrorAlerts alerts = new ErrorAlerts();
-  static Logger logger = new Logger();
-  static DexUI initUI = new DexUI();
+  public static ErrorAlerts alerts = new ErrorAlerts();
+  public static Logger logger = new Logger();
+  public static DexUI preloaderUI = new DexUI();
 
 
   /**
    * Dynamic label change in UI with additional logging.
-   * @param text the text to be shown and logged.
+   * @param ui the DexUI instance
+   * @param mainLabelText the main label text (used to the logger aswell)
+   * @param secLabelText the secondary label text. It can't be null but can be empty.
    */
-  public static void changeStatus(String text)
+  public static void changeStatus(DexUI ui, String mainLabelText, String secLabelText)
   {
-    initUI.changeMainLabel(text);
-    logger.log("INFO", text);
+    ui.changeMainLabel(mainLabelText);
+    if (!secLabelText.equals(""))
+    {
+      ui.changeSecondaryLabel(secLabelText);
+    }
+    logger.log("INFO", mainLabelText);
   }
 
   /**
@@ -69,6 +77,9 @@ public class Init extends Application
   public void start(Stage primaryStage) throws Exception
   {
     alerts.setImage(new Image(Init.class.getResourceAsStream("icon1.jpg")));
+    //DexCraft Commons alerts binding
+    Commons.setErrorAlerts(alerts);
+
     FXMLLoader splashLoader = new FXMLLoader(getClass().getResource("Preloader.fxml"));
     StackPane splashPane = splashLoader.load();
     preloaderStage = new Stage(StageStyle.TRANSPARENT);
@@ -83,14 +94,19 @@ public class Init extends Application
     preloaderStage.setResizable(false);
     pbar = new ProgressBar(0.0);
     preloaderLabel = new Label("Iniciando...");
+    preloaderLabel.getStyleClass().add("mainlabel");
+    preloaderLabel.setMaxSize(480, 20);
+    preloaderLabel.setTranslateY(135);
+    preloaderLabel.setAlignment(Pos.CENTER);
+    preloaderLabel.toFront();
+    pbar.setMaxSize(480, 11);
+    pbar.setTranslateY(150);
+    pbar.getStylesheets().add(getClass().getResource("gradientprogressbar2.css").toExternalForm());
+    pbar.toFront();
     splashPane.getChildren().add(preloaderLabel);
     splashPane.getChildren().add(pbar);
-    preloaderLabel.getStyleClass().add("mainlabel");
-    pbar.setMaxSize(606, 11);
-    pbar.setTranslateY(190);
-    pbar.getStylesheets().add(getClass().getResource("gradientprogressbar2.css").toExternalForm());
-    initUI.setProgressBar(pbar);
-    initUI.setMainLabel(preloaderLabel);
+    preloaderUI.setProgressBar(pbar);
+    preloaderUI.setMainLabel(preloaderLabel);
 
 
     /** Opens a Service to show Splash before initialize the application **/
@@ -124,46 +140,65 @@ public class Init extends Application
             logger.setLogNameFormat("yyyy-MM-dd--HH.mm.ss");
             logger.setLogDir(DexCraftFiles.logFolder);
             logger.log("INFO", "Logger inicializado.");
+            //DexCraft Commons logger binding
+            Commons.setLogger(logger);
 
-            changeStatus("Iniciando...");
-            AdminExecution.AdminExecution();
-            initUI.changeProgress(true, 10, 40);
+            changeStatus(preloaderUI, "Iniciando...", "");
+
+            preloaderUI.changeProgress(true, 10, 30);
             logger.log("INFO", "Preparando Cache...");
+
+            // Prepares the program cache folder and files
             Validate.cache();
-            initUI.changeProgress(true, 20, 40);
+
+            preloaderUI.changeProgress(true, 20, 30);
+
+            // Do tasks below only if launcher isn't on Offline Mode
             if(!OfflineMode.IsRunning())
             {
-              initUI.changeProgress(true, 30, 40);
-              changeStatus("Baixando Corefile...");
+              preloaderUI.changeProgress(true, 30, 30);
+              changeStatus(preloaderUI, "Baixando Corefile...", "");
+
+              //Performs CoreFile download
               Download downloadCf = new Download();
               downloadCf.coreFile();
-              initUI.changeProgress(true, 40, 40);
-              changeStatus("Verificando o sistema. Aguarde...");
+
+              preloaderUI.changeProgress(true, 40, 30);
+              changeStatus(preloaderUI, "Verificando o sistema. Aguarde...", "");
+
+              //Check System Requirements
               SystemRequirements req = new SystemRequirements();
               req.checkRequirements();
-              initUI.changeProgress(true, 50, 40);
-              changeStatus("Verificando recursos...");
-              Validate.resources();
-              initUI.changeProgress(true, 70, 40);
-              changeStatus("Verificando versão do DexCraft Launcher...");
-              Validate.provisionedComponent(DexCraftFiles.coreFile, DexCraftFiles.launcherProperties, "DexCraft Launcher",
-                       "DexCraftLauncherVersion", "Versions", "LauncherProperties", "LauncherUpdates", "DCLUpdate",
-                       DexCraftFiles.tempFolder, DexCraftFiles.updateLauncherZip, DexCraftFiles.launcherFolder);
-              initUI.changeProgress(true, 80, 40);
-              changeStatus("Verificando versão do DexCraft Background Services...");
-              Validate.provisionedComponent(DexCraftFiles.coreFile, DexCraftFiles.launcherProperties, "DexCraft Background Services",
-                       "DexCraftBackgroundServicesVersion", "Versions", "LauncherProperties", "LauncherUpdates", "DCBSUpdate",
-                       DexCraftFiles.tempFolder, DexCraftFiles.updateDCBSZip, DexCraftFiles.launcherFolder);
+
+              preloaderUI.changeProgress(true, 50, 30);
+              changeStatus(preloaderUI, "Verificando recursos...", "");
+
+              // Check if resouce folder is downloaded and updated
+              Validate.provisionedComponent(preloaderUI, "Resources", 60);
+
+              preloaderUI.changeProgress(true, 70, 30);
+              changeStatus(preloaderUI, "Verificando versão do DexCraft Launcher...", "");
+
+              // Check and update Client version if needed
+              Validate.provisionedComponent(preloaderUI, "Client", 80);
+
+              changeStatus(preloaderUI, "Verificando versão do DexCraft Background Services...", "");
+
+              // Check and update DCBS version if needed
+              Validate.provisionedComponent(preloaderUI, "DCBS", 90);
             }
-            initUI.changeProgress(true, 90, 40);
+            preloaderUI.changeProgress(true, 95, 30);
+
+            // Interrupt Launcher if CoreFile is absent
             if(!DexCraftFiles.coreFile.exists())
             {
               logger.log("***ERRO***", "COREFILE NÃO ENCONTRADO.");
               alerts.noCoreFile();
             }
-            changeStatus("Abrindo DexCraft Launcher...");
-            initUI.changeProgress(true, 100, 40);
-            logger.log("INFO", "Splash Screen terminada");
+
+            changeStatus(preloaderUI, "Abrindo DexCraft Launcher...", "");
+            preloaderUI.changeProgress(true, 100, 30);
+            logger.log("INFO", "JAVAFX: Splash Screen terminada");
             return true;
           }
         };
@@ -196,7 +231,7 @@ public class Init extends Application
   {
     new ProcessBuilder("cmd", "/c", "C:\\DexCraft\\launcher\\bin\\javaw.exe -jar DexCraftLauncher.jar").directory(DexCraftFiles.launcherFolder).start();
     logger.log("INFO", "Inicializando DexCraft Launcher...");
-    Close.close(0);
+    Close.init();
   }
 
 }
